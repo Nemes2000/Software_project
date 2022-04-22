32c5aacc20ad47a58630425e31e5d2da3e5d9443
A.P. <aranypeter20@gmail.com>
2022-04-22 19:15:35 +0200

    Beradance

--- src/whut/Beardance.java
+++ src/whut/Beardance.java
@@ -1,5 +1,21 @@
-package tee;
+package whut;
 
-public class Beardance {
-
+public class Beardance extends Agens{
+	public boolean startTurnEffect(Virologus v) {
+		infectAll(v.getField().getVirologusok(),v);
+		v.move(v.getField().getNeighborhood().);
+		infectAll(v.getField().getVirologusok(),v);		
+		return false;
+	}
+	public void destroyEffect(Packet p) {
+		p.handlePossiblyLostMaterial(p.getMaxMaterial());
+	}
+	public void infectAll(ArrayList<Virologus> vs, Virologus v) {
+		for(Virologus vc : vs) {
+			Beardance b = new Beardance();
+			v.addAgens(b);
+			vc.uRAttacked(b,v);
+			
+		}
+	}
 }
