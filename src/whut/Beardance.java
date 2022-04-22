e8f756281608fb6770666b176aa905b2299e95ec
A.P. <aranypeter20@gmail.com>
2022-04-22 19:18:11 +0200

    fix

--- src/whut/Beardance.java
+++ src/whut/Beardance.java
@@ -1,21 +1,23 @@
 package whut;
 
 public class Beardance extends Agens{
+	@Override
 	public boolean startTurnEffect(Virologus v) {
 		infectAll(v.getField().getVirologusok(),v);
-		v.move(v.getField().getNeighborhood().);
+		v.move(v.getField().getNeighborhood().get(0));
 		infectAll(v.getField().getVirologusok(),v);		
 		return false;
 	}
+	@Override
 	public void destroyEffect(Packet p) {
 		p.handlePossiblyLostMaterial(p.getMaxMaterial());
 	}
+	@Override
 	public void infectAll(ArrayList<Virologus> vs, Virologus v) {
 		for(Virologus vc : vs) {
 			Beardance b = new Beardance();
 			v.addAgens(b);
-			vc.uRAttacked(b,v);
-			
+			vc.uRAttacked(b,v);		
 		}
 	}
 }
