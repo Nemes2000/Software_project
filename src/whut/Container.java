4b9ad5bf0f5359f591c769a17cddd7263791de72
A.P <aranypeter20@gmail.com>
2022-05-08 15:17:59 +0200

    dolgok

--- src/whut/Container.java
+++ src/whut/Container.java
@@ -1,13 +1,22 @@
 package whut;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Container extends JPanel {
     private ArrayList<Icon> icons = new ArrayList<Icon>();
+    private String containerText = new String();
 
     public void addIcon(Icon i){
         icons.add(i);
+    }
+
+    public void draw()
+    {
+        this.removeAll();
+        this.setLayout(new FlowLayout());
+
     }
 
 
